18eb64ae06558bcb92ca4672a115aaca346541a6
Amy Kapernick <amy@aimhigherweb.design>
2021-07-05 15:23:16 +0800

    removed old config

--- next.config.js
+++ next.config.js
@@ -18,36 +18,6 @@ const moduleExports = {
 			@use "styles/_mixins.scss";
 		`,
 	},
-	webpack: (config, options) => {
-		// Netlify Plugin fix
-		if (config.externals) {
-			config.externals.push(`@netlify/zip-it-and-ship-it`);
-		} else {
-			config.externals = [`@netlify/zip-it-and-ship-it`];
-		}
-
-		// Inline SVGs
-		config.module.rules.push({
-			test: /\.svg$/,
-			use: [
-				{
-					loader: `@svgr/webpack`,
-					options: {
-						svgoConfig: {
-							plugins: [
-								{ cleanupIDs: false },
-								{ prefixIds: false },
-								{ moveGroupAttrsToElems: false }
-							]
-						}
-					}
-				}
-			],
-
-		});
-
-		return config;
-	  },
 };
 
 const SentryWebpackPluginOptions = {
