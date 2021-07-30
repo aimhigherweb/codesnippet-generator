2d7b8202c5cb0b914987fea210e0194b993f9735
Amy Kapernick <amy@aimhigherweb.design>
2021-07-30 17:29:12 +0800

    fixed slider and adjusted cta colours

--- components/blocks/slider/code.js
+++ components/blocks/slider/code.js
@@ -4,7 +4,7 @@ import Product from './single';
 import { getData } from '../../../utils/data';
 
 const uuid = Math.floor(Math.random() * 100) + Math.floor(Math.random() * 100);
-const distance = 270;
+const distance = 334;
 
 const ProductSliderCode = ({ data }) => {
 	const products = data;
@@ -45,7 +45,7 @@ const ProductSliderCode = ({ data }) => {
 };
 
 export const ProductSliderCodeScript = () => (
-	<script>{`document.querySelectorAll("#sliderControls_${uuid} button").forEach(e=>{e.addEventListener("click",e=>{let t=e.target;"svg"==t.tagName&&(t=t.parentElement);const o=t.dataset.direction,l=document.querySelector("#slider_${uuid}"),r=l.scrollLeft,s=l.scrollLeftMax;console.log({opt:t,direction:o,slider:l,position:r,max:s});let i=r;"forwards"==o&&(i=r+${distance}),"backwards"==o&&(i=r-${distance}),console.log(i),l.scroll({left:i,behaviour:"smooth"});const n=document.querySelector("#sliderControls_${uuid}"),a=n.querySelector(".next button"),d=n.querySelector(".previous button"),c=e=>{e.disabled=!0,e.setAttribute("aria-disabled",!0)},u=e=>{e.disabled=!1,e.setAttribute("aria-disabled",!1)};console.log({controls:n,next:a,previous:d,disableButton:c,enableButton:u,newPosition:i,max:s}),i>=s?c(a):u(a),i<=0?c(d):u(d)})});`}</script>
+	<script>{`const sliderScroll=e=>{const t=document.querySelector("#slider_${uuid}"),r=t.scrollLeft,l=t.scrollLeftMax;let o=r;"forwards"==e&&(0==(o=${distance}*Math.ceil(r/${distance}))?o=${distance}:o==r&&(o=r+${distance})),"backwards"==e&&(o=${distance}*Math.floor(r/${distance}))==r&&(o=r-${distance}),t.scroll({left:o,behaviour:"smooth"});const c=document.querySelector("#sliderControls_${uuid}"),s=c.querySelector(".next button"),a=c.querySelector(".previous button"),d=e=>{e.disabled=!0,e.setAttribute("aria-disabled",!0)},i=e=>{e.disabled=!1,e.setAttribute("aria-disabled",!1)};o>=l?d(s):i(s),o<=0?d(a):i(a)};document.querySelectorAll("#sliderControls_${uuid} button").forEach(e=>{e.addEventListener("click",e=>{let t=e.target;"svg"==t.tagName&&(t=t.parentElement),sliderScroll(t.dataset.direction)})});`}</script>
 );
 
 export default ProductSliderCode;
