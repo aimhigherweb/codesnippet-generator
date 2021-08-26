057ba1b2b12bf657fb2741908b6614834b8a3bef
Amy Kapernick <amy@aimhigherweb.design>
2021-08-26 13:35:09 +0800

    fixed banenr for blank slide

--- utils/parse/banner.js
+++ utils/parse/banner.js
@@ -8,26 +8,34 @@ const parseProduct = (data) => {
 	const bannerItems = container.querySelector(`.pa_banner_home`);
 	const navItems = container.querySelector(`.pa_banner_nav ul`);
 
-	for (const banner of bannerItems?.children) {
+	console.log(bannerItems.querySelectorAll('li[data-slide]'));
+
+	for (const banner of bannerItems.querySelectorAll('li[data-slide]')) {
 		const link = banner.querySelector(`a`)?.href;
 		const image = banner.querySelector(`img`)?.src;
 
-		banners.push({
-			image,
-			link
-		});
+		console.log({link, image});
+
+		if(image && image !== '') {
+			banners.push({
+				image,
+				link
+			});
+		}
+
+		
 	}
 
 	for (const navItem of navItems?.children) {
 		const link = navItem.querySelector(`a`)?.href;
 		const { text } = navItem.querySelector(`a`)?.dataset;
 
-		console.log(navItem.querySelector(`a`));
-
-		navs.push({
-			text,
-			link
-		});
+		if(text && text !== '') {
+			navs.push({
+				text,
+				link
+			});
+		}
 	}
 
 	console.log({ banners, navs });
