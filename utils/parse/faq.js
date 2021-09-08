1710dee6916436f6b7c0a99da05b0c65cc1c2fc6
Amy Kapernick <amy@aimhigherweb.design>
2021-09-08 11:58:31 +0800

    updated faq parsing to log results for error tracking

--- utils/parse/faq.js
+++ utils/parse/faq.js
@@ -1,9 +1,10 @@
 const parseFaq = (data) => {
 	const faqs = [];
 	// eslint-disable-next-line no-restricted-syntax
+	console.log({ data, children: data.children });
 	for (faq of data.children) {
-		const summary = faq.querySelector(`[data-attribute='summary']`).innerHTML;
-		const content = faq.querySelector(`[data-attribute='content']`).innerHTML;
+		const summary = faq?.querySelector(`[data-attribute='summary']`)?.innerHTML;
+		const content = faq?.querySelector(`[data-attribute='content']`)?.innerHTML;
 
 		faqs.push({
 			summary,
